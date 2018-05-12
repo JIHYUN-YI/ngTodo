2a9c6626200e48710f30508374aca88c7f5054a4
JIHYUN-YI <mellowsun@hanmail.net>
2018-05-12 10:54:25 +0900

    관리자사이트: 머티리얼 디자인에서 제공하는 paginator 생성

--- src/app/domain/page.vo.ts
+++ src/app/domain/page.vo.ts
@@ -1,7 +1,7 @@
 export class PageVO {
-  constructor(public pageIndex: number, public pageSize: number, public totalCount: number, public pageOptions?: number[]) {
-    if (!pageOptions) {
-      pageOptions = [5, 15, 30, 60, 120];
+  constructor(public pageIndex: number, public pageSize: number, public totalCount: number, public pageSizeOptions?: number[]) {
+    if (!pageSizeOptions) {
+      pageSizeOptions = [5, 15, 30, 60, 120];
     }
   }
 }
