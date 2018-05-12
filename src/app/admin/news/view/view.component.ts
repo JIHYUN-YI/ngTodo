9bf39e0bb3b62334962d696975bb7d6c74fbc891
JIHYUN-YI <mellowsun@hanmail.net>
2018-05-12 12:22:00 +0900

    관리자사이트: 뉴스 상세보기 서비스 호출
    DomSanitizer로 html을 래핑하여 style 태그 가져옴
    => 사진이 모바일에서 크게 맞게 보이게 된다.

--- src/app/admin/news/view/view.component.ts
+++ src/app/admin/news/view/view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AdminService} from '../../admin.service';
+import {NewsVO} from '../../../domain/news.vo';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-view',
@@ -8,8 +10,10 @@ import {AdminService} from '../../admin.service';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
+  news: NewsVO;
+  html: SafeHtml;
 
-  constructor(private route: ActivatedRoute, private adminService: AdminService) {
+  constructor(private route: ActivatedRoute, private adminService: AdminService, private sanitizer: DomSanitizer) {
     // view 객체를 호출할 때, 첫 번째만 생성, 두 번째부터는 찍히지 않는다.
     // => 한 번만 생성
     console.log(location.pathname);
@@ -26,6 +30,8 @@ export class ViewComponent implements OnInit {
     this.adminService.findOneNews(news_id)
       .subscribe(body => {
         console.log(body);
+        this.news = body;
+        this.html = this.sanitizer.bypassSecurityTrustHtml(this.news.content);
     });
   }
 
